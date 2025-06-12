bb6d1786bcd6d528c31a2cddae728958a78fac55
Rodrigo Gastaud <rodrigogastaud_97@hotmail.com>
2025-06-11 22:20:19 -0300

    adição da div de dentro do chaveamento

--- app/home/jogos/index.tsx
+++ app/home/jogos/index.tsx
@@ -1,5 +1,34 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacityProps } from "react-native";
 import Jogos from "@/app/components/Jogos";
+import { Button } from "@/app-example/components/button";
+import { StyleSheet } from "react-native";
+
+const torneio1 = {
+  data: "10/06/2022",
+  jogadores: "",
+};
+
+const ChaveA = [
+  { direita: "Lebron", esquerda: "Tapia" },
+  { direita: "Chingotto", esquerda: "Franco" },
+  { direita: "Leandro", esquerda: "Gastaud" },
+  { direita: "Braga", esquerda: "Rota" },
+  { direita: "Tuan", esquerda: "Denis" },
+];
+
+const ChaveB = [
+  { direita: "Lebron", esquerda: "Tapia" },
+  { direita: "Chingotto", esquerda: "Franco" },
+  { direita: "Leandro", esquerda: "Gastaud" },
+  { direita: "Braga", esquerda: "Rota" },
+];
+
+const ChaveC = [
+  { direita: "Lebron", esquerda: "Tapia" },
+  { direita: "Chingotto", esquerda: "Franco" },
+  { direita: "Leandro", esquerda: "Gastaud" },
+  { direita: "Braga", esquerda: "Rota" },
+];
 
 export default function JogosScreen() {
   return (
@@ -11,8 +40,28 @@ export default function JogosScreen() {
         backgroundColor: "#2C2C2C",
       }}
     >
-      <Text>sdf</Text>
-      <Jogos />
+      <View style={styles.divNovoTorneio}>
+        <Text>Componente para selecionar a data</Text>
+        <Button title="Novo Torneio" />
+      </View>
+      <View style={styles.divEdicaoGatorade}>
+        <Text>Jogo: DATA</Text>
+        <Button title="Editar" />
+      </View>
+      <Jogos chave="A" />
+      <Jogos chave="B" />
+      <Jogos chave="C" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  divNovoTorneio: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  divEdicaoGatorade: {
+    display: "flex",
+    flexDirection: "row",
+  },
+});
